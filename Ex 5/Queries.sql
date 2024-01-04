368394508fe44b1adc97e7fa9966e6a8e4326291
Shaganashree P L <120777273+Shaganashree97@users.noreply.github.com>
2024-01-04 09:52:39 +0530

    Create Queries.sql

---
+++ Ex 5/Queries.sql
@@ -0,0 +1,86 @@
+--	Display all the Instructor id and Instructor name from the Instructor table.
+SELECT IID, Iname FROM Instructor;
+/*
+Output
++-----+----------+
+| IID | Iname    |
++-----+----------+
+|   1 | Princy   |
+|   2 | Swathi   |
+|   3 | Arun     |
+|   4 | Shanthi  |
+|   5 | Anushree |
++-----+----------+
+*/
+
+--	Write a query to display the building where department is CSE.
+SELECT Building FROM Department WHERE Dname = 'CSE';
+/*
+Output
++----------+
+| Building |
++----------+
+| A Block  |
++----------+
+*/
+
+--	List all the faculty members of IT department.
+
+/*
+Output
+
+*/
+--	Write a query to display the department name of the student with ID = 76653
+
+/*
+Output
+
+*/
+--	Update the location of ECE dept to “B” Block.
+
+/*
+Output
+
+*/
+--	List instructor whose salary is between 40000 and 70000
+
+/*
+Output
+
+*/
+--	Display CSE student whose credit earned in greater than 50.
+
+/*
+Output
+
+*/
+--	Display the instructor names starting with “S”.
+
+/*
+Output
+
+*/
+--	Display the Student names of length 5.
+
+/*
+Output
+
+*/
+--	Update the total credits earned by “William” to 65.
+
+/*
+Output
+
+*/
+--	List the departments in “A” block whose budget is Greater than one lakh. 
+
+/*
+Output
+
+*/
+--	List the Teacher who belong to “IT” and “CSE”.
+
+/*
+Output
+
+*/
